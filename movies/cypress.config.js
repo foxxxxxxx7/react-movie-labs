e0e52ae2c23986640094d6cc92b98be874f89ba0
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-11-05 18:33:22 +0000

    Revert "E2E tests - Navigation test spec."
    This reverts commit 998d63e1acf0b2139f7f965c0135637d38936c35.

--- movies/cypress.config.js
+++ movies/cypress.config.js
@@ -1,11 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
   e2e: {
     "baseUrl": "http://localhost:3000/",
-    "viewportWidth": 1980,
-    "viewportHeight": 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
